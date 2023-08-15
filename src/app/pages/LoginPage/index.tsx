5dba07d8cc9f10fa80820ba7c3ac96b0ab21464b
loc <nguyengialoc7@gmail.com>
2023-08-15 15:27:40 +0700

    Login Page

--- src/app/pages/LoginPage/index.tsx
+++ src/app/pages/LoginPage/index.tsx
@@ -1,7 +1,43 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
+import {
+  Container,
+  TextInput,
+  PasswordInput,
+  Text,
+  Paper,
+  Group,
+  Button,
+  Divider,
+  Checkbox,
+  Anchor,
+  Stack,
+} from '@mantine/core';
+import { useToggle, upperFirst } from '@mantine/hooks';
+import { useForm } from '@mantine/form';
+import {
+  GoogleButton,
+  TwitterButton,
+} from '../../../components/SocialButtons/SocialButton';
 
 export function LoginPage() {
+  const [type, toggle] = useToggle(['login', 'register']);
+  const form = useForm({
+    initialValues: {
+      email: '',
+      name: '',
+      password: '',
+      terms: true,
+    },
+
+    validate: {
+      email: val => (/^\S+@\S+$/.test(val) ? null : 'Invalid email'),
+      password: val =>
+        val.length <= 6
+          ? 'Password should include at least 6 characters'
+          : null,
+    },
+  });
   return (
     <>
       <Helmet>
@@ -11,7 +47,95 @@ export function LoginPage() {
           content="A Boilerplate application LoginPage"
         />
       </Helmet>
-      <span>My LoginPage</span>
+      <Container size={'xs'}>
+        {' '}
+        <Paper radius="md" p="xl">
+          <Text size="lg" weight={500}>
+            Welcome to Mantine, {type} with
+          </Text>
+
+          <Group grow mb="md" mt="md">
+            <GoogleButton radius="xl">Google</GoogleButton>
+            <TwitterButton radius="xl">Twitter</TwitterButton>
+          </Group>
+
+          <Divider
+            label="Or continue with email"
+            labelPosition="center"
+            my="lg"
+          />
+
+          <form onSubmit={form.onSubmit(() => {})}>
+            <Stack>
+              {type === 'register' && (
+                <TextInput
+                  label="Name"
+                  placeholder="Your name"
+                  value={form.values.name}
+                  onChange={event =>
+                    form.setFieldValue('name', event.currentTarget.value)
+                  }
+                  radius="md"
+                />
+              )}
+
+              <TextInput
+                required
+                label="Email"
+                placeholder="hello@mantine.dev"
+                value={form.values.email}
+                onChange={event =>
+                  form.setFieldValue('email', event.currentTarget.value)
+                }
+                error={form.errors.email && 'Invalid email'}
+                radius="md"
+              />
+
+              <PasswordInput
+                required
+                label="Password"
+                placeholder="Your password"
+                value={form.values.password}
+                onChange={event =>
+                  form.setFieldValue('password', event.currentTarget.value)
+                }
+                error={
+                  form.errors.password &&
+                  'Password should include at least 6 characters'
+                }
+                radius="md"
+              />
+
+              {type === 'register' && (
+                <Checkbox
+                  label="I accept terms and conditions"
+                  checked={form.values.terms}
+                  onChange={event =>
+                    form.setFieldValue('terms', event.currentTarget.checked)
+                  }
+                />
+              )}
+            </Stack>
+
+            <Group position="apart" mt="xl">
+              <Anchor
+                component="button"
+                type="button"
+                color="dimmed"
+                onClick={() => toggle()}
+                size="xs"
+              >
+                {type === 'register'
+                  ? 'Already have an account? Login'
+                  : "Don't have an account? Register"}
+              </Anchor>
+              <Button type="submit" radius="xl">
+                {upperFirst(type)}
+              </Button>
+            </Group>
+          </form>
+        </Paper>
+      </Container>
     </>
   );
 }
